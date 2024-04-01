ca79483185893e7a7f655e8cc0648f6f65c5b17c
han <386275556@qq.com>
2024-04-01 22:03:28 +0800

    init

---
+++ demo01/build/CMakeFiles/demo01.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/han/code/cmake_learn/cmake/demo01/add.c" "/home/han/code/cmake_learn/cmake/demo01/build/CMakeFiles/demo01.dir/add.c.o"
+  "/home/han/code/cmake_learn/cmake/demo01/div.c" "/home/han/code/cmake_learn/cmake/demo01/build/CMakeFiles/demo01.dir/div.c.o"
+  "/home/han/code/cmake_learn/cmake/demo01/main.c" "/home/han/code/cmake_learn/cmake/demo01/build/CMakeFiles/demo01.dir/main.c.o"
+  "/home/han/code/cmake_learn/cmake/demo01/mult.c" "/home/han/code/cmake_learn/cmake/demo01/build/CMakeFiles/demo01.dir/mult.c.o"
+  "/home/han/code/cmake_learn/cmake/demo01/sub.c" "/home/han/code/cmake_learn/cmake/demo01/build/CMakeFiles/demo01.dir/sub.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ demo01/build/CMakeFiles/demo01.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo01.dir/add.c.o"
+  "CMakeFiles/demo01.dir/div.c.o"
+  "CMakeFiles/demo01.dir/main.c.o"
+  "CMakeFiles/demo01.dir/mult.c.o"
+  "CMakeFiles/demo01.dir/sub.c.o"
+  "demo01.pdb"
+  "demo01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
